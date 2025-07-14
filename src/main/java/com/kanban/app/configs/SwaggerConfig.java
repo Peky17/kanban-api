ef90aacd477b9fcdcfd26ddaada853956cba311a
Enrique Bernardo González Vázquez <ebgvdeveloper@gmail.com>
2025-07-13 21:29:54 -0600

    feat: Enhance Swagger configuration to include API info and contact details

--- src/main/java/com/kanban/app/configs/SwaggerConfig.java
+++ src/main/java/com/kanban/app/configs/SwaggerConfig.java
@@ -5,20 +5,31 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+
 // Enable JWT Authentication in Swagger
 @Configuration
 public class SwaggerConfig {
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")));
-    }
+        @Bean
+        public OpenAPI customOpenAPI() {
+                return new OpenAPI()
+                                .info(new Info()
+                                                .title("Kanban Board API")
+                                                .description("Kanban Management API")
+                                                .version("1.0.0")
+                                                .contact(new Contact()
+                                                                .name("Enrique González")
+                                                                .email("ebgvdeveloper@gmail.com")
+                                                                .url("https://peky17.github.io/ebgv-portfolio-web/portfolio/")))
+                                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                                .components(new Components()
+                                                .addSecuritySchemes("bearerAuth",
+                                                                new SecurityScheme()
+                                                                                .type(SecurityScheme.Type.HTTP)
+                                                                                .scheme("bearer")
+                                                                                .bearerFormat("JWT")));
+        }
 }
